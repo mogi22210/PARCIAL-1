a159f37b534f99286ccc569692a0ebfb8cffc069
mogi22210 <159710709+mogi22210@users.noreply.github.com>
2024-03-06 19:00:41 -0500

    CODE

---
+++ bike.py
@@ -0,0 +1,4 @@
+class Bike:
+    def __init__(self, bike_id, available=True):
+        self.bike_id = bike_id
+        self.available = available
---
+++ equipment.py
@@ -0,0 +1,3 @@
+class Equipment:
+    def __init__(self, name):
+        self.name = name
