f178b4387cce7c0212b7ca86432f6c6186dde741
mogi22210 <159710709+mogi22210@users.noreply.github.com>
2024-03-08 15:20:19 -0500

    Delete library.py

--- library.py
+++
@@ -1,6 +0,0 @@
-class Library:
-    def __init__(self):
-        self.books = ["Introduction to Python", "Data Structures and Algorithms", "Machine Learning"]
-
-    def check_book_availability(self, book):
-        return book in self.books
