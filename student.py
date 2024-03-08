69ee56b40acc3bdba00ca992621cdc7b66d85b04
mogi22210 <159710709+mogi22210@users.noreply.github.com>
2024-03-08 15:20:34 -0500

    Delete student.py

--- student.py
+++
@@ -1,6 +0,0 @@
-class Student:
-    def __init__(self, student_id, name, user, password):
-        self.student_id = student_id
-        self.name = name
-        self.user = user
-        self.password = password
